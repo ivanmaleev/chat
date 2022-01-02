ab9bc9b10442fece1c3ffb0dab398760be463d4e
ivanmaleev@inbox.ru <mudr6PHEL9$>
2022-01-02 16:49:01 +0300

    3. Авторизация JWT [#9146]

--- src/main/java/ru/job4j/chat/ChatApplication.java
+++ src/main/java/ru/job4j/chat/ChatApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ChatApplication extends SpringBootServletInitializer {
@@ -15,6 +17,11 @@ public class ChatApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
